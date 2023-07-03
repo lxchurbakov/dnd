847f026bb0754f375f012e79579774445e3f0aff
lxchurbakov <lxchurbakov@gmail.com>
2023-07-03 10:35:46 +0300

    React starter

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SomePage from './page';
+
+const app = document.getElementById('app');
+
+ReactDOM.render((
+    <SomePage />
+), app);
