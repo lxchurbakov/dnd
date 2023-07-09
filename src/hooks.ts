a288fcbd0474db050faace227b89146112a2b2d3
lxchurbakov <lxchurbakov@gmail.com>
2023-07-09 10:11:25 +0300

    Put hooks to a separate file

---
+++ src/hooks.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const useEventListener = (o, name, cb, deps) => {
+    React.useEffect(() => {
+        o.addEventListener(name, cb);
+
+        return () => o.removeEventListener(name, cb);
+    }, deps);
+};
